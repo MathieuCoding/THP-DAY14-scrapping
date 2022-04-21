54d185c0ce107044225dd34118939a1fe557e24f
matt <matthepaul@yandex.com>
2022-04-21 17:09:38 +0200

    mairie_christmas modified

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -16,8 +16,9 @@ def get_townhall_urls
     main_page = "https://www.annuaire-des-mairies.com/"
     page = Nokogiri::HTML(URI.open("http://annuaire-des-mairies.com/val-d-oise.html"))
     page.xpath('//a[@class="lientxt"]/@href').each do |url|
-    return array_urls << main_page + url.content[2..-1]
+    array_urls << main_page + url.content[2..-1]
     end
+    return array_urls
 end
 
 #Method to get names of each cities 
@@ -26,17 +27,32 @@ def get_townhall_names
     page = Nokogiri::HTML(URI.open("https://www.annuaire-des-mairies.com/val-d-oise.html"))
     page.xpath('//*[@class="lientxt"]').each do |node|
     names << node.content.split.map(&:capitalize).join(' ')
-    return names
     end
+    return names
 end
 
 #Method to get email addresses from all cities
 def get_all_emails
-    page = Nokogiri::HTML(URI.open(get_townhall_urls.to_s))
-    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |email|
-        return  email.text
+    array_mails = []
+    get_townhall_urls.each do |url|
+    main = Nokogiri::HTML(URI.open(url))
+    main.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |all|
+    array_mails << all.text
     end
+    end
+    return array_mails
 end
+
+#Method to combine both previous arrays
+def get_final_array
+    puts hash_contacts = Hash[get_all_emails.zip(get_townhall_names)]
+end
+
+#    page = Nokogiri::HTML(URI.open(get_townhall_urls.to_s))
+#    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').each do |email|
+#    end
+#    return  email.text
+#end
 
 
 
@@ -45,3 +61,4 @@ get_townhall_email
 get_townhall_urls
 get_townhall_names
 get_all_emails
+get_final_array
